ea89690699e97b6290170b6c8cdd40489ab120a4
Kaleidophon <dennis.ulmer@altechnologies.de>
2016-05-11 18:48:33 +0200

    Added function to check TransE data integrity and remove duplicates.

--- src/trans_e/partition_data.py
+++ src/trans_e/partition_data.py
@@ -7,11 +7,14 @@ def main():
 	argparser = init_argparse()
 	args = argparser.parse_args()
 	print args
-	prts = (float(args.partitions[0]), float(args.partitions[1]), float(args.partitions[2]))
-	assert prts[0] + prts[1] + prts[2] == 1.0
+	if args.uniqueness_check:
+		check_data_integrity(args.input, args.remove_clones, args.outdir)
+	else:
+		prts = (float(args.partitions[0]), float(args.partitions[1]), float(args.partitions[2]))
+		assert prts[0] + prts[1] + prts[2] == 1.0
 
-	data = read_relations(args.input)
-	partition_data(data, prts, args.outdir)
+		data = read_relations(args.input)
+		partition_data(data, prts, args.outdir)
 
 
 def read_relations(inpath):
@@ -25,7 +28,7 @@ def write_data_in_file(data, file):
 
 
 def partition_data(data, prts, outdir):
-	#random.shuffle(data)
+	random.shuffle(data)
 	size = len(data)
 
 	train_file = codecs.open(outdir + "freebase_mtr100_mte100-train.txt", 'wb', 'utf-8')
@@ -45,18 +48,55 @@ def partition_data(data, prts, outdir):
 	test_file.close()
 
 
+def check_data_integrity(data_inpath, remove_clones, outpath):
+	"""
+	Check whether all triplets in the data are unique.
+	"""
+	triplets = set()
+
+	with codecs.open(data_inpath, 'rb', 'utf-8') as infile:
+		line = infile.readline().strip()
+		while line:
+			parts = line.split("\t")
+			triplet = (parts[0], parts[1], parts[2])
+			if triplet in triplets:
+				sys.stderr.write("Triplet (%s, %s, %s) already in data!\n" %(triplet[0], triplet[1], triplet[2]))
+				if not remove_clones:
+					sys.exit(0)
+			else:
+				triplets.add(triplet)
+			line = infile.readline().strip()
+
+	print "Check successful!"
+
+	if remove_clones:
+		print "Writing new data..."
+		with codecs.open(outpath, 'wb', 'utf-8') as outfile:
+			for triplet in triplets:
+				outfile.write("%s\t%s\t%s\n" %(triplet[0], triplet[1], triplet[2]))
+
+
+def check_set_integrity(indir):
+	pass
+	# Check whether all relation in validation and test set appear at least once in training set
+
+
 def init_argparse():
 	argparser = argparse.ArgumentParser()
 	argparser.add_argument('--input',
 							required=True,
 							help='Relation input data')
 	argparser.add_argument('--outdir',
-							required=True,
 							help='Output data directory')
 	argparser.add_argument('--partitions',
 							nargs=3,
-							required=True,
 							help="Percentages of training / validation / testset")
+	argparser.add_argument('--uniqueness_check',
+							action="store_true",
+							help="Checks the uniqueness of input data triplets.")
+	argparser.add_argument('--remove_clones',
+							action='store_true',
+							help="Removes clones while checking for uniqueness.")
 	return argparser
 
 if __name__ == '__main__':
